40a4b83eb8f68fff07eaa513817a60277cfd2d53
Nauman Tahir <nomibzu@gmail.com>
2014-09-19 18:47:50 +0500

    Update MineSweeper.rb

--- bin/MineSweeper.rb
+++ bin/MineSweeper.rb
@@ -19,33 +19,25 @@ class MineSweeper
 	
 	def before_show()
 
-			topLevelWindow = @builder["window1"] 													#getting topLevel Window
-			#topLevelWindow.set_size_request @rows * 50, @cols * 50 				#setting its width and height
-			table = Gtk::Fixed.new																				# Creating a Fixed Container To Place Buttons						
-			
-			# creating buttons and attaching them to Fixed Container
-			for i in 0..@rows-1
-					tempButtons = []
-					for j in 0..@cols-1
-						myButton = Gtk::Button.new()
-						myButton.builder_name = i.to_s + "," +j.to_s
-						myButton.set_size_request 30, 30
-						table.put myButton, i*30, j*30
-						myButton.show
-						tempButtons.push(myButton)
-					end
-					@buttons.push(tempButtons)
-
-				
-
-			end	
-			table.add_events(Gdk::Event::BUTTON_PRESS_MASK)						#to catch right click event (it catches all button clicks)
-			topLevelWindow.add(table)																			#Adding Table to Top Level Window
-			prepare_board
-			bind_handlers																					#Binding Handlers to each button
-
-			
-					
+		topLevelWindow = @builder["window1"] 									#getting topLevel Window
+		table = Gtk::Fixed.new													# Creating a Fixed Container To Place Buttons
+		# creating buttons and attaching them to Fixed Container
+		for i in 0..@rows-1
+			tempButtons = []
+			for j in 0..@cols-1
+				myButton = Gtk::Button.new()
+				myButton.builder_name = i.to_s + "," +j.to_s
+				myButton.set_size_request 30, 30
+				table.put myButton, i*30, j*30
+				myButton.show
+				tempButtons.push(myButton)
+			end
+			@buttons.push(tempButtons)
+		end	
+		table.add_events(Gdk::Event::BUTTON_PRESS_MASK)						#to catch right click event (it catches all button clicks)
+		topLevelWindow.add(table)											#Adding Table to Top Level Window
+		prepare_board
+		bind_handlers														#Binding Handlers to each button
 	end
 
 	def prepare_board
@@ -86,7 +78,7 @@ class MineSweeper
 						for y in j-1..j+1
 							if x>=0 && y >=0 && x < @rows && y < @cols
 								if @board[x][y]==-1
-								count=count+1
+									count=count+1
 								end
 							end
 						end
@@ -100,55 +92,48 @@ class MineSweeper
 
 	def bind_handlers
 		#Binding Handlers to Each Button
-			@buttons.each do |buttonArray| 
-			  buttonArray.each do |button|				
-
-						
-						button.signal_connect("button_press_event") do |widget, event|
-							#menu.popup(nil, nil, event.button, event.time)
-			 				if (event.button == 3)
-									if button.alignment == [0,0]
-                  	button.image = nil
-										button.set_alignment(0.5, 0.5)
-									else
-									  image = Gtk::Image.new ("bin/data/media/flag.jpg")
-										#button.add(image)
-										button.image= image	
-										#image.show
-										button.set_alignment(0, 0)
-									
-									end	
-							end				
+		@buttons.each do |buttonArray| 
+		    buttonArray.each do |button|				
+				button.signal_connect("button_press_event") do |widget, event|
+ 					if (event.button == 3)
+						if button.alignment == [0,0]
+	              			button.image = nil
+							button.set_alignment(0.5, 0.5)
+						else
+							image = Gtk::Image.new ("bin/data/media/flag.jpg")
+		   					#button.add(image)
+							button.image= image	
+							#image.show
+							button.set_alignment(0, 0)
 						end	
-				
-						
-						button.signal_connect('clicked') do |btn|
-
-							@clicks = clicks + 1
-							@builder["window1"].title = @clicks.to_s 
-							index = btn.builder_name.split(',')
-							x = index[0].to_i
-							y = index[1].to_i							
-							if @board[x][y] == -1                    #if clicked button is a mine...end game
-								image = Gtk::Image.new ("bin/data/media/minered.jpg")
-								@buttons[x][y].image = image	
-								#image.show				
-								show_mines
-								
-							else
-								btn.label = @board[x][y].to_s
-								btn.sensitive= false
-								if @mines.length - 10 == @rows * @cols - @clicks			# if all mines spotted correctly, game won.. 
-									show_mines 
-									@builder["window1"].title = @clicks.to_s + "-You win!!"						
-								end						
-								uncover(x,y) if @board[x][y] == 0    #uncover sorrounding area of clicked button if no mines found in its adjacent neighbours
-								                    
-							end						
-						end
+					end				
 				end	
+				button.signal_connect('clicked') do |btn|
+				@clicks = clicks + 1
+				@builder["window1"].title = @clicks.to_s 
+				index = btn.builder_name.split(',')
+				x = index[0].to_i
+				y = index[1].to_i							
+				if @board[x][y] == -1                    #if clicked button is a mine...end game
+					image = Gtk::Image.new ("bin/data/media/minered.jpg")
+					@buttons[x][y].image = image	
+					#image.show				
+					show_mines
+					
+				else
+					btn.label = @board[x][y].to_s
+					btn.sensitive= false
+					if @mines.length - 10 == @rows * @cols - @clicks			# if all mines spotted correctly, game won.. 
+						show_mines 
+						@builder["window1"].title = @clicks.to_s + "-You win!!"						
+					end						
+					uncover(x,y) if @board[x][y] == 0    #uncover sorrounding area of clicked button if no mines found in its adjacent neighbours
+			                    
+				end						
 			end
-	end	
+		end	
+	end
+end	
 
 	def show_mines	 
 		@builder["window1"].title = @clicks.to_s + "-Game Over" 
@@ -163,53 +148,35 @@ class MineSweeper
 		end
 	end
 
-#Uncovering the clicked button's neighbours if none of them contains a mine
+	#Uncovering the clicked button's neighbours if none of them contains a mine
 
-def uncover (i,j)		
+	def uncover (i,j)		
 		x_array = [i]       #array containing x-indexes of clicked button which have 0 mines in the sorrounding
 		y_array = [j]				#y-index of clicked button  "    "    "    "    "    "    "    "     "     "    "
-
+	
 		x_array.each_with_index do |n,index|	
 			i = n
 			j = y_array[index]
-			
-		 for x in i-1..i+1
-		 	for y in j-1..j+1
-				if x>=0 && y >=0 && x < @rows && y < @cols
-					next if @buttons[x][y].state == "insensitive"				# next iteration if this array index(button) has already been uncovered
-					@buttons[x][y].label = @board[x][y].to_s					
-					@buttons[x][y].sensitive = false
-
-					@clicks = @clicks + 1       #click count will be updated for every array index traversed
-					@builder["window1"].title = @clicks.to_s
-
-					if @board[x][y] == 0         #if neighbours of the clicked button contain another 0 valued button,we will add that to our array
-						@board[x][y] = "-"						
-				  	x_array << x								#if another 0 is found in the neighbours, insert it in to uncover array
-						y_array << y                # same as above, saving y-index 
-					end
-				end
-			end
-		 end 
-		end
-	end
-
-	def reuncover (i,j)		
-		#uncover_array=1		
-		if @board[i][j]!=-1
 			for x in i-1..i+1
-				for y in j-1..j+1
+			 	for y in j-1..j+1
 					if x>=0 && y >=0 && x < @rows && y < @cols
-											
-							@buttons[x][y].label = @board[x][y].to_s					
-							@buttons[x][y].sensitive= false
-							#uncover(x,y) if @board[x][y] == 0						
-						
+						next if @buttons[x][y].state == "insensitive"				# next iteration if this array index(button) has already been uncovered
+						@buttons[x][y].label = @board[x][y].to_s					
+						@buttons[x][y].sensitive = false
+						@clicks = @clicks + 1       #click count will be updated for every array index traversed
+						@builder["window1"].title = @clicks.to_s
+						if @board[x][y] == 0         #if neighbours of the clicked button contain another 0 valued button,we will add that to our array
+							@board[x][y] = "-"						
+					  		x_array << x								#if another 0 is found in the neighbours, insert it in to uncover array
+							y_array << y                # same as above, saving y-index 
+						end
 					end
 				end
-			end 
+			 end 
 		end
 	end
+
+	
 
 	def button_clicked(widget, event)
      # which button was clicked?
@@ -296,6 +263,25 @@ def uncover (i,j)		 #vol 3
 				end
 			end 
 	end
+	
+	def reuncover (i,j)		
+		#uncover_array=1		
+		if @board[i][j]!=-1
+			for x in i-1..i+1
+				for y in j-1..j+1
+					if x>=0 && y >=0 && x < @rows && y < @cols
+											
+							@buttons[x][y].label = @board[x][y].to_s					
+							@buttons[x][y].sensitive= false
+							#uncover(x,y) if @board[x][y] == 0						
+						
+					end
+				end
+			end 
+		end
+	end
+	
+	
 eb = Gtk::EventBox.new
 w.add(eb)
 #eb.add_events(Gdk::Event::ALL_EVENTS_MASK)
