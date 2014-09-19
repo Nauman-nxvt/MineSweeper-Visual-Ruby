3b1f026db5eb004787132fd9938964bb312da60c
Nauman Tahir <nomibzu@gmail.com>
2014-09-19 18:54:22 +0500

    Update MineSweeper.rb

--- bin/MineSweeper.rb
+++ bin/MineSweeper.rb
@@ -18,8 +18,8 @@ class MineSweeper
 	end
 	
 	def before_show()
-		topLevelWindow = @builder["window1"] 									#getting topLevel Window
-		table = Gtk::Fixed.new													# Creating a Fixed Container To Place Buttons
+		topLevelWindow = @builder["window1"] 								#getting topLevel Window
+		table = Gtk::Fixed.new												# Creating a Fixed Container To Place Buttons
 		# creating buttons and attaching them to Fixed Container
 		for i in 0..@rows-1
 			tempButtons = []
@@ -58,7 +58,7 @@ class MineSweeper
 		for i in 0..no_of_mines-1
 			x = rand(@rows)
 			y = rand(@cols)
-			redo if @board[x][y] == -1     # to prevent repitition of mines on same index
+			redo if @board[x][y] == -1     										# to prevent repitition of mines on same index
 			@board[x][y] = -1
 			@mines << x
 			@mines << y 
@@ -80,7 +80,6 @@ class MineSweeper
 						end
 					end
 					@board[i][j] = count
-					#buttons[i][j].label = @board[i][j].to_s
 				end
 			end
 		end
@@ -108,7 +107,7 @@ class MineSweeper
 				index = btn.builder_name.split(',')
 				x = index[0].to_i
 				y = index[1].to_i							
-				if @board[x][y] == -1                    #if clicked button is a mine...end game
+				if @board[x][y] == -1                    						#if clicked button is a mine...end game
 					image = Gtk::Image.new ("bin/data/media/minered.jpg")
 					@buttons[x][y].image = image	
 					#image.show				
@@ -121,7 +120,7 @@ class MineSweeper
 						show_mines 
 						@builder["window1"].title = @clicks.to_s + "-You win!!"						
 					end						
-					uncover(x,y) if @board[x][y] == 0    #uncover sorrounding area of clicked button if no mines found in its adjacent neighbours
+					uncover(x,y) if @board[x][y] == 0    						#uncover sorrounding area of clicked button if no mines found in its adjacent neighbours
 			                    
 				end						
 			end
@@ -143,40 +142,29 @@ end
 
 	#Uncovering the clicked button's neighbours if none of them contains a mine
 
-	def uncover (i,j)		
-		x_array = [i]       #array containing x-indexes of clicked button which have 0 mines in the sorrounding
-		y_array = [j]				#y-index of clicked button  "    "    "    "    "    "    "    "     "     "    "
+	def uncover (i,j)			
+		x_array = [i]       											#array containing x-indexes of clicked button which have 0 mines in the sorrounding
+		y_array = [j]													#y-index of clicked button  "    "    "    "    "    "    "    "     "     "    "
 		x_array.each_with_index do |n,index|	
 			i = n
 			j = y_array[index]
 			for x in i-1..i+1
 			 	for y in j-1..j+1
 					if x>=0 && y >=0 && x < @rows && y < @cols
-						next if @buttons[x][y].state == "insensitive"				# next iteration if this array index(button) has already been uncovered
+						next if @buttons[x][y].state == "insensitive"	# next iteration if this array index(button) has already been uncovered
 						@buttons[x][y].label = @board[x][y].to_s					
 						@buttons[x][y].sensitive = false
-						@clicks = @clicks + 1       #click count will be updated for every array index traversed
+						@clicks = @clicks + 1       					#click count will be updated for every array index traversed
 						@builder["window1"].title = @clicks.to_s
-						if @board[x][y] == 0         #if neighbours of the clicked button contain another 0 valued button,we will add that to our array
+						if @board[x][y] == 0         					#if neighbours of the clicked button contain another 0 valued button,we will add that to our array
 							@board[x][y] = "-"						
 					  		x_array << x								#if another 0 is found in the neighbours, insert it in to uncover array
-							y_array << y                # same as above, saving y-index 
+							y_array << y                				# same as above, saving y-index 
 						end
 					end
 				end
 			 end 
 		end
-	end
-
-	def button_clicked(widget, event)
-     # which button was clicked?
-     #if event.button == 1
-      #   p "left click"
-     #else if event.button == 2
-      #   p "middle click"
-      if event.button == 3
-         p "right click"
-		 end
 	end
 
 	#On Destroy event Handler	
